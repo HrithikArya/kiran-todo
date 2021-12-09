87720fad090ecda727005b42df08848c29d7d7a4
Ritik kumar <hrithikarya@github.com>
2021-12-09 11:44:24 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import React from 'react'
+import { Todos } from './Mycomponents/Todos';
+import { AddTodo } from './Mycomponents/AddTodo';
+import { Navbar } from './Mycomponents/Navbar';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  let initTodo;
+  if (localStorage.getItem("todos") === null)
+    initTodo = [];
+  else
+    initTodo = JSON.parse(localStorage.getItem("todos"));
 
+
+  const onDelete = (todo) => {
+    settodos(todos.filter(
+      (e) => {
+        return e != todo;
+      }
+    ))
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+
+  const addTodo = (title, desc) => {
+    let sno;
+    if (todos.length == 0)
+      sno = 1;
+    else sno = todos[todos.length - 1].sno + 1;
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    console.log(myTodo);
+    settodos([...todos, myTodo]);
+  }
+
+  const [todos, settodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+
+  return (
+    <>
+      <Navbar />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+    </>
+  )
+};
 export default App;
